448a6cd555f58c6716b5caa7f472b642c548b0fb
Vuxpn <vuphannguyen15102003@gmail.com>
2024-07-30 17:09:42 +0700

    Add initial player component with UI elements: sidebar, play song

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Sidebar from './components/sidebar';
+import Player from './components/player';
 
 const App = () => {
     return (
         <div className="bg-black h-screen">
-            <div className="h- [90%] flex">
+            <div className="h-[90%] flex">
                 <Sidebar />
             </div>
+            <Player />
         </div>
     );
 };
