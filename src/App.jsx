a1073c3e59b41ab330ad85b6017b39b63a7cac16
Vuxpn <vuphannguyen15102003@gmail.com>
2024-07-30 11:34:08 +0700

    set up prj

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sidebar from './components/sidebar';
+
+const App = () => {
+    return (
+        <div className="bg-black h-screen">
+            <div className="h- [90%] flex">
+                <Sidebar />
+            </div>
+        </div>
+    );
+};
+
+export default App;
